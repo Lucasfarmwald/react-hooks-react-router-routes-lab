ff2cddd4c77c05932cc69a52db9fc05a8e6d9f61
lucasfarmwald <farmwaldlucas@gmail.com>
2022-02-16 00:33:59 -0600

    finished Directors.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,7 +6,7 @@ function Movies() {
 const movieItems = movies.map((movie) => (
   <div key={movie.title}>
     <h2>{movie.title}</h2>
-    <p>Runtime:{movie.time} min.</p>
+    <p>Runtime: {movie.time} min.</p>
     <ul>
       {movie.genres.map((genre) => (
      <li key={genre}>{genre}</li>
