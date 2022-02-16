436658124cf954cdb35aba8ec47235659d2a0599
lucasfarmwald <farmwaldlucas@gmail.com>
2022-02-16 00:26:47 -0600

    finished Actors.js

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,10 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
+
+const actorItems = actors.map((actor) => (
+  <div key={actor.name}>
+    <h2>{actor.name}</h2>
+    <ul key={actor.movies}>{actor.movies.map((movie) => (
+     <li key={movie} >{movie}</li>
+    ))}</ul>
+  </div>
+))
+
+
   return <div>
-    <h1> 
-      Actors Page
-    </h1>
+    <h1>Actors Page</h1>
+    {actorItems}
   </div>;
 }
 
